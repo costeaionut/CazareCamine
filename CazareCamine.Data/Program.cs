63e1518091662d3037addab648989652122cdefa
Ionut-Andrei Costea <costeacionut@gmail.com>
2021-09-30 11:38:58 +0300

    Added Data project for the solution

---
+++ CazareCamine.Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CazareCamine.Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
