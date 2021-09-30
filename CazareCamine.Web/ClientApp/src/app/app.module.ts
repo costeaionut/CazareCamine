20e6484ae7e8c572196d0f910426cd13deb344a6
Ionut-Andrei Costea <costeacionut@gmail.com>
2021-09-30 13:40:50 +0300

    Install EF Core, Bootstrap and JWT Bearer libraries

--- CazareCamine.Web/ClientApp/src/app/app.module.ts
+++ CazareCamine.Web/ClientApp/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { AuthenticationModule } from '../authentication/authentication.module';
+import { LoginComponent } from '../authentication/login/login.component';
+import { RegisterComponent } from '../authentication/register/register.component';
 
 @NgModule({
   declarations: [
@@ -22,10 +25,13 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    AuthenticationModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent }
     ])
   ],
   providers: [],
